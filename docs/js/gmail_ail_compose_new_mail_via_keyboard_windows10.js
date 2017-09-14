b1fc37d2d3419b277a019e7d13817c8390b0513e
MarkYan <mark11yan@gmail.com>
2017-09-14 12:41:12 +0800

    auto deploy on 12:41:12

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -61,5 +61,6 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 13), 122.220000],
 			[Date.UTC(2017, 8, 13), 133.330000],
 			[Date.UTC(2017, 8, 13), 166.670000],
+			[Date.UTC(2017, 8, 13), 133.330000],
 		]    }]
 }
--- docs/js/gmail_ail_type_in_reply_field_windows10.js
+++ docs/js/gmail_ail_type_in_reply_field_windows10.js
@@ -59,5 +59,6 @@ var gmail_ail_type_in_reply_field_windows10 = {
 			[Date.UTC(2017, 8, 13), 22.220000],
 			[Date.UTC(2017, 8, 13), 22.220000],
 			[Date.UTC(2017, 8, 13), 5.560000],
+			[Date.UTC(2017, 8, 13), 5.560000],
 		]    }]
 }
